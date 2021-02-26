2aeeb4ac3675cb5a716197e06ddfc18331b164e0
Daniel Tutistar <danieltutistar@gmail.com>
2021-02-26 05:17:41 -0500

    esena completa de juego

---
+++ Assets/scripts/vidaItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vidaItem : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Cojer()
+    {
+        animator.SetTrigger("cojer");
+        gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+        Invoke("Destruir", 0.5f);
+    }
+
+    private void Destruir()
+    {
+        Destroy(this.gameObject);
+    }
+}
