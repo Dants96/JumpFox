b8f6f147456ddaa095908a9e08c0697642d41e8a
Daniel Tutistar <danieltutistar@gmail.com>
2021-02-26 07:05:26 -0500

    final v1

---
+++ Assets/scripts/casaScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class casaScript : MonoBehaviour
+{
+    public Text textoInfo;
+    
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.CompareTag("jugador")) {
+            textoInfo.enabled = true;
+            Invoke("MostrarCreditos", 2);
+            
+
+        }
+    }
+
+    private void MostrarCreditos(){
+        SceneManager.LoadScene(2);
+    }
+}
